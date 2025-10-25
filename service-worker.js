864a624e16b49ee1da07eef98eb0aba19e061a6a
msiteenterprises2016-svg <msite.enterprises2016@gmail.com>
2025-10-25 12:51:24 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,31 @@
+
 const CACHE_NAME = 'msite-cache-v1';
 const urlsToCache = [
-  'index.html',
-  'msite.png',
-  'manifest.json',
+  './',
+  './index.html',
+  './manifest.json',
+  './msite.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(()=>caches.match('index.html')))
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.mode === 'navigate') {
+    event.respondWith(
+      fetch(req).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(req).then(res => res || fetch(req))
   );
 });
